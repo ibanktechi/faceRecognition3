58d629898c05a18dcc360cd3c800125bc31f2b31
ibanktechi <ibtechi@outlook.com>
2021-08-31 00:27:08 +0800

    requirements

---
+++ detect.py
@@ -0,0 +1,100 @@
+import time
+from pathlib import Path
+
+import cv2
+import torch
+
+from models.experimental import attempt_load
+from utils.datasets import LoadImages
+from utils.general import non_max_suppression,  apply_classifier, scale_coords
+from utils.plots import Annotator, colors
+from utils.torch_utils import select_device, load_classifier
+
+@torch.no_grad()
+def run(weights='yolov5s.pt',  # model.pt path(s)
+        source='data/images',  # file/dir/URL/glob, 0 for webcam
+        imgsz=640,  # inference size (pixels)
+        conf_thres=0.25,  # confidence threshold
+        iou_thres=0.45,  # NMS IOU threshold
+        max_det=1000,  # maximum detections per image
+        device='cpu',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        save_crop=False,  # save cropped prediction boxes
+        classes=None,  # filter by class: --class 0, or --class 0 2 3
+        agnostic_nms=False,  # class-agnostic NMS
+        augment=False,  # augmented inference
+        line_thickness=3,  # bounding box thickness (pixels)
+        hide_labels=False,  # hide labels
+        hide_conf=False,  # hide confidences
+        half=False,  # use FP16 half-precision inference
+        ):
+
+    # Initialize
+    device = select_device(device)
+    half &= device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    w = weights[0] if isinstance(weights, list) else weights
+    classify, suffix = False, Path(w).suffix.lower()
+    pt, onnx, tflite, pb, saved_model = (suffix == x for x in ['.pt', '.onnx', '.tflite', '.pb', ''])  # backend
+    stride, names = 64, [f'class{i}' for i in range(1000)]  # assign defaults
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+    stride = int(model.stride.max())  # model stride
+    names = model.module.names if hasattr(model, 'module') else model.names  # get class names
+    if half:
+        model.half()  # to FP16
+    if classify:  # second-stage classifier
+        modelc = load_classifier(name='resnet50', n=2)  # initialize
+        modelc.load_state_dict(torch.load('resnet50.pt', map_location=device)['model']).to(device).eval()
+
+    dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt)
+
+    t0 = time.time()
+    for path, img, im0s, vid_cap in dataset:
+        if onnx:
+            img = img.astype('float32')
+        else:
+            img = torch.from_numpy(img).to(device)
+            img = img.half() if half else img.float()  # uint8 to fp16/32
+        img = img / 255.0  # 0 - 255 to 0.0 - 1.0
+        if len(img.shape) == 3:
+            img = img[None]  # expand for batch dim
+
+        # Inference
+        if pt:
+            pred = model(img, augment=augment)[0]
+
+        # NMS
+        pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+
+        # Second-stage classifier (optional)
+        if classify:
+            pred = apply_classifier(pred, modelc, img, im0s)
+
+        # Process predictions
+        for i, det in enumerate(pred):  # detections per image
+            p, s, im0, frame = path, '', im0s.copy(), getattr(dataset, 'frame', 0)
+
+            p = Path(p)  # to Path
+            s += '%gx%g ' % img.shape[2:]  # print string
+            imc = im0.copy() if save_crop else im0  # for save_crop
+            annotator = Annotator(im0, line_width=line_thickness, pil=not ascii)
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    c = int(cls)
+                    label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
+                    annotator.box_label(xyxy, label, color=colors(c, True))
+
+            im0 = annotator.result()
+            cv2.imshow(str(p), im0)
+
+run()
+
